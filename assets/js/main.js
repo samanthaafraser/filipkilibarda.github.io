999e9ba7473e3a1c74890f8abf9dcb46b7f4b98d
Filip Kilibarda <filipcsc@gmail.com>
2020-06-01 21:26:08 -0700

    Updated main.js to do scrollspy stuff

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,4 +14,37 @@ $(document).ready(function() {
 
   // add lightbox class to all image links
   $("a[href$='.jpg'], a[href$='.png'], a[href$='.gif']").attr("data-lity", "");
+
+  // Gumshoe scroll spy init
+  if($("nav.toc").length > 0) {
+    var spy = new Gumshoe("nav.toc a", {
+      // Active classes
+      navClass: "active", // applied to the nav list item
+      contentClass: "active", // applied to the content
+
+      // Nested navigation
+      nested: false, // if true, add classes to parents of active link
+      nestedClass: "active", // applied to the parent items
+
+      // Offset & reflow
+      offset: 20, // how far from the top of the page to activate a content area
+      reflow: true, // if true, listen for reflows
+
+      // Event support
+      events: true // if true, emit custom events
+    });
+  }
+
+  // Add anchors for headings
+  $('.page__content').find('h1, h2, h3, h4, h5, h6').each(function() {
+    var id = $(this).attr('id');
+    if (id) {
+      var anchor = document.createElement("a");
+      anchor.className = 'header-link';
+      anchor.href = '#' + id;
+      anchor.innerHTML = '<span class=\"sr-only\">Permalink</span><i class=\"fa fa-link\"></i>';
+      anchor.title = "Permalink";
+      $(this).append(anchor);
+    }
+  });
 });
